c213891b11d6598f1b969203cff97f168965af3f
tom-englert <mail@tom-englert.de>
2017-07-28 13:22:48 +0200

    Add XML-documentation for attributes

--- Throttle/ThrottleDefaultImplementationAttribute.cs
+++ Throttle/ThrottleDefaultImplementationAttribute.cs
@@ -2,13 +2,26 @@
 
 namespace Throttle
 {
+    /// <summary>
+    /// Allows you to specify a default for the throttle implementation on assembly or class level, 
+    /// so you don't have to specify it in every <see cref="ThrottledAttribute"/>.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Assembly)]
     public class ThrottleDefaultImplementationAttribute : Attribute
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ThrottleDefaultImplementationAttribute"/> class.
+        /// </summary>
+        /// <param name="implementation">The type of the throttle implementation.</param>
         public ThrottleDefaultImplementationAttribute(Type implementation)
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ThrottleDefaultImplementationAttribute"/> class.
+        /// </summary>
+        /// <param name="implementation">The type of the throttle implementation.</param>
+        /// <param name="methodName">Name of the method to call on the implementation to trigger the throttle.</param>
         public ThrottleDefaultImplementationAttribute(Type implementation, string methodName)
         {
         }
--- Throttle/ThrottleDefaultThresholdAttribute.cs
+++ Throttle/ThrottleDefaultThresholdAttribute.cs
@@ -2,9 +2,17 @@
 
 namespace Throttle
 {
+    /// <summary>
+    /// Allows you to specify a default for the throttle threshold on assembly or class level, 
+    /// so you don't have to specify it in every <see cref="ThrottledAttribute"/>.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Assembly)]
     public class ThrottleDefaultThresholdAttribute : Attribute
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ThrottleDefaultThresholdAttribute"/> class.
+        /// </summary>
+        /// <param name="threshold">The default threshold.</param>
         public ThrottleDefaultThresholdAttribute(int threshold)
         {
         }
