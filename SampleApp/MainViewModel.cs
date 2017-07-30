cac5712b652757008f7c39837e40dc52ca079130
tom-englert <mail@tom-englert.de>
2017-07-30 15:14:21 +0200

    Add SampleApp to solution, provide some more realistic samples

---
+++ SampleApp/MainViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+using PropertyChanged;
+
+using Throttle;
+
+using TomsToolbox.Core;
+using TomsToolbox.Desktop;
+using TomsToolbox.Wpf;
+
+namespace SampleApp
+{
+    [AddINotifyPropertyChangedInterface]
+    public class MainViewModel
+    {
+        public ObservableCollection<string> Items { get; } = new ObservableCollection<string>();
+        public ObservableCollection<string> Events { get; } = new ObservableCollection<string>();
+
+        public MainViewModel()
+        {
+            Items.CollectionChanged += (_, __) => Items_CollectionChanged();
+
+            Items.AddRange(Enumerable.Range(1, 5)
+                .Select(i => "Item " + i));
+        }
+
+        private void Items_CollectionChanged()
+        {
+            Events.Add("Items_CollectionChanged");
+
+            Refresh1();
+            Refresh2();
+        }
+
+        public string Text { get; set; }
+
+        public ICommand LoadedCommand => new DelegateCommand(() => Events.Add("Window loaded"));
+
+        [Throttled(typeof(DispatcherThrottle))]
+        private void Refresh1()
+        {
+            Events.Add("Refresh 1 called");
+        }
+
+        [Throttled(typeof(DispatcherThrottle), (int)DispatcherPriority.Input)]
+        private void Refresh2()
+        {
+            Events.Add("Refresh 2 called");
+        }
+
+        [Throttled(typeof(TomsToolbox.Desktop.Throttle), 500)]
+        private void OnTextChanged()
+        {
+            Events.Add($"Start searching for \"{Text}\"");
+        }
+    }
+}
--- SampleApp/MainWindow.xaml.cs
+++ SampleApp/MainWindow.xaml.cs
@@ -12,33 +12,11 @@ namespace SampleApp
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
-        private int _index;
-
         public MainWindow()
         {
             InitializeComponent();
-
-            Test();
-            Test();
-            Test();
-            Test();
-        }
-
-        [Throttled(typeof(DispatcherThrottle), (int)DispatcherPriority.Input)]
-        public async void Test()
-        {
-            await Task.Delay(TimeSpan.FromSeconds(2));
-
-            Title = $"Test {++_index}";
-        }
-
-        private async void Self_Loaded(object sender, RoutedEventArgs e)
-        {
-            await Task.Delay(TimeSpan.FromSeconds(1));
-
-            Title = "Test - Loaded";
         }
     }
 }
