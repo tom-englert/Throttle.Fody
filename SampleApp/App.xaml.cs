ffd24a37dc7a37040d8bd291b2a346a4fd65e52d
tom-englert <mail@tom-englert.de>
2017-08-01 18:41:54 +0200

    Pimp up sample window

--- SampleApp/App.xaml.cs
+++ SampleApp/App.xaml.cs
@@ -1,9 +1,19 @@
-﻿namespace SampleApp
+﻿using System.Windows;
+
+using TomsToolbox.Wpf.Styles;
+
+namespace SampleApp
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            Resources.MergedDictionaries.Insert(0, WpfStyles.Defaults());
+        }
     }
 }
